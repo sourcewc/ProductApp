654741c9a20866a385b57d14255ea1437432de6d
Shin <sourcewc222@gmail.com>
2016-11-29 22:15:17 +0900

    Third commit -WebView Html linking - using ZXING but Portratemode , in FrameLayout View required - menu Item //menifest file FirstActivity is ->.activity.MainActivity

--- app/src/main/java/com/jainjang/activity/QRResultActivity.java
+++ app/src/main/java/com/jainjang/activity/QRResultActivity.java
@@ -3,10 +3,14 @@ package com.jainjang.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import com.jainjang.R;
 
@@ -15,7 +19,6 @@ import com.jainjang.R;
  * 큐알코드 결과 액티비티
  */
 public class QRResultActivity extends Activity {
-    private static final String URL_TO_LOAD = "http://google.com";
     WebView mWebView;
 
     @Override
@@ -25,6 +28,8 @@ public class QRResultActivity extends Activity {
 
         ImageView qnaImage = (ImageView) findViewById(R.id.qna_btn);
         ImageView reviewImage = (ImageView) findViewById(R.id.review_btn);   //리뷰,qna 이벤트용
+        ImageView qrMore=(ImageView)findViewById(R.id.qr_more); //더보기 버튼
+
 
         qnaImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,26 +46,58 @@ public class QRResultActivity extends Activity {
                 startActivity(intent);
             }
         });
+        qrMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnClick(view);
+            }
+        });
 
 
-        //웹뷰 부분 html 커스텀 필요
+
+        //웹뷰 부분 html 파일필요 html소스는 main/assets 안에
 
         mWebView = (WebView)findViewById(R.id.result_web);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://google.com");
-        mWebView.setWebViewClient(new WebViewClientClass());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setSaveFormData(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(false);
+
+        mWebView.loadUrl("file:///android_asset/HelloWorld.html");
+
+
     }
 
-    //shoudOverideUrlLoding 이외의 다른것 찾아봐야됨
-    class WebViewClientClass extends WebViewClient{
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+    //옵션 생성, 셀렉트
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.qr_result_menu, menu);
+        return true;
+
+    }
+    public void mOnClick(View v) {
+        PopupMenu popup = new PopupMenu(this,v);
+        MenuInflater inflater =popup.getMenuInflater();
+        Menu menu= popup.getMenu();
+        inflater.inflate(R.menu.qr_result_menu,menu);
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_itemqna:
+                        return true;
+                    case R.id.menu_request_stamp:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popup.show();
     }
 
-
-
+    /////////////////
 }
 
